7a40f23db5574e0756b81ba96ea8c4000219b58f
Kevin Riquelme <kriquelme10@gmail.com>
2024-12-17 11:21:56 -0300

    resolviendo conflictos con archivos plugins y middlewares

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,26 +1,8 @@
-module.exports = [
+export default [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          // De nuevo, no se incluye la URL porque no se tiene aún
-          'connect-src': ["'self'"], // Solo permite las conexiones al mismo origen por ahora
-        },
-      },
-    },
-  },
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      // Permite solicitudes desde tu frontend local mientras desarrollas
-      origin: ['http://localhost:3000'], // Reemplázalo después con la URL de tu despliegue
-    },
-  },
+  'strapi::security',
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
