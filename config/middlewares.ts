83406441aa7e8582410786e2e578e4b6d03d01ad
Kevin Riquelme <kriquelme10@gmail.com>
2024-12-17 09:28:29 -0300

    config database

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,24 @@
-export default [
+module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https://dr-viandas-bck-2.onrender.com'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:3000'], // Permite tu frontend local
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+// config/middleware.js o config/plugins.js
+
+module.exports = {
+      settings: {
+        cors: {
+          enabled: true,
+          origin: ['http://localhost:3000', 'https://tu-frontend.com'], // Asegúrate de poner aquí las URLs correctas
+        },
+      },
+    };
+    
