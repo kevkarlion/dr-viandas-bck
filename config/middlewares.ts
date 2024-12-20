3789d89d68b428ce91e4e9726af2bc7e1d1ebbf1
Kevin Riquelme <kriquelme10@gmail.com>
2024-12-20 16:49:08 -0300

    config middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,35 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: [
+        "https://mi-frontend-dominio.com", // Reemplaza con tu dominio frontend en producción
+        "http://localhost:3001", // Reemplaza si estás probando localmente
+      ],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      headers: ["Content-Type", "Authorization"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
+
+// export default [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
