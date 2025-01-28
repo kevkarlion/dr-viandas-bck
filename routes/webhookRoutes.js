4e7a57d925a204a498ee8b8a796194e8a6647da6
Kevin Riquelme <kriquelme10@gmail.com>
2025-01-28 11:21:06 -0300

    url webhook nva

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const { handleWebhook } = require('../controllers/webhookController');
 const router = express.Router();
 
-router.post('/webhook', handleWebhook);
+router.post('/date', handleWebhook);
 
 module.exports = router;
