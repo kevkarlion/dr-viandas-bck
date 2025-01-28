5f9078f12ffd558d959327c47de1087061fc6939
Kevin Riquelme <kriquelme10@gmail.com>
2025-01-28 20:08:13 -0300

    add token

--- services/consultarPago.js
+++ services/consultarPago.js
@@ -1,12 +1,15 @@
 const axios = require("axios");
 
+
+const accessToken = process.env.MERCADOPAGO_ACCESS_TOKEN
+
 const consultarPago = async (paymentId) => {
   try {
     const response = await axios.get(
       `https://api.mercadopago.com/v1/payments/${paymentId}`,
       {
         headers: {
-          Authorization: `Bearer TU_ACCESS_TOKEN`,
+          Authorization: accessToken,
         },
       }
     );
